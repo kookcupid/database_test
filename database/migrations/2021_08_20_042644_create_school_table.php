7087bce6ddf3318d5750914ee87beab505b17e52
kookcupid <kookcupid@gmail.com>
2021-08-20 12:02:10 +0700

    update migrate table school

---
+++ database/migrations/2021_08_20_042644_create_school_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchoolTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('school', function (Blueprint $table) {
+            $table->id();
+            $table->string('fname');
+            $table->string('lname');
+            $table->char(1)('sex');
+            $table->char(1)('class');  
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('school');
+    }
+}
